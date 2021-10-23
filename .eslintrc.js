e0138bcee516817a08f91e7920b15b1906af17f1
James Halstead <jameshalstead637@gmail.com>
2021-10-22 23:21:08 -0700

    Made some progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,11 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "comma-dangle": ["error", "never"],
+        "comma-dangle": "off",
+        "react/prop-types": "off",
+          // suppress errors for missing 'import React' in files
+   "react/react-in-jsx-scope": "off",
+   // allow jsx syntax in js files (for next.js project)
+  "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
     }
 };
--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Expenses from './components/Expenses';
+
+// import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Groceries",
+      amount: 394.12,
+      date: new Date(2021, 7, 15),
+    },
+    { 
+      id: "e2", 
+      title: "Internet", 
+      amount: 79.49, 
+      date: new Date(2021, 2, 12) 
+    },
+    {
+      id: "e3",
+      title: "Phone bill",
+      amount: 294.67,
+      date: new Date(2021, 5, 28),
+    },
+    {
+      id: "e4",
+      title: "Suitecases",
+      amount: 450,
+      date: new Date(2021, 10, 12),
+    },
+    {
+      id: "e5",
+      title: "Airline tickets",
+      amount: 3240,
+      date: new Date(2021, 10, 15),
+    },
+    {
+      id: "e6",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: "e7",
+      title: "Hotel",
+      amount: 1150,
+      date: new Date(2021, 9, 12),
+    },
+    {
+      id: "e8",
+      title: "Activities",
+      amount: 750,
+      date: new Date(2021, 11, 12),
+    },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+    <Expenses items={expenses}/>
+   
+    </>
   );
 }
 
